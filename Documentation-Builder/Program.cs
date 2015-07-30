1b284ca9f2c892bb5085298c32faf2b71a2d50e5
ThadHouse <thad@athometech.com>
2015-07-29 18:38:40 -0700

    Changes to documentation builder
    I need to build a better release system. most code in here will get
    scrapped when i do that.

--- Documentation-Builder/Program.cs
+++ Documentation-Builder/Program.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using BitPantry.AssemblyPatcher;
+using LibGit2Sharp;
 using NuGet;
 
 namespace Documentation_Builder
@@ -15,10 +17,80 @@ namespace Documentation_Builder
         //This tool will be used to download all of the nuget packages we have public, and generate documentation for them using sandcastle. 
         //It will also generate redirection HTML files so we can go directly to an individual namespace.
         //This can be hardcoded since we will not be adding new repositories often.
+        
+        //This is being redone since we need to clone the repository
+
+        static void Main(string[] args)
+        {
+            //First we need to clone our 2 repositories
+            CloneOptions options = new CloneOptions();
+            Repository.Clone("https://github.com/robotdotnet/robotdotnet-wpilib.git", "robotdotnet-wpilib", options);
+
+            options = new CloneOptions();
+            Repository.Clone("https://github.com/robotdotnet/NetworkTablesDotNet.git", "NetworkTablesDotNet", options);
+
+
+            //Next we need to grab the latest version numbers
+            List<string> packageIDs = new List<string>()
+            {
+                "FRC.NetworkTables",
+                "FRC.WPILib",
+            };
+            IPackageRepository repo =
+                PackageRepositoryFactory.Default.CreateRepository("https://packages.nuget.org/api/v2");
+
+            var packages = repo.FindPackages(packageIDs);
+            packages = packages.Where(item => item.IsReleaseVersion() && item.IsLatestVersion).ToList();
+
+            string WPILibVersion = "";
+            string NTVersion = "";
+
+            foreach (var package in packages)
+            {
+                if (package.Title == "FRC WPILib")
+                {
+                    WPILibVersion = package.Version.ToString();
+                }
+                if (package.Title == "FRC NetworkTables")
+                {
+                    NTVersion = package.Version.ToString();
+                }
+            }
+
+            //Patch NetworkTables
+            if (NTVersion == "")
+                throw new InvalidOperationException("NT version not set");
+            string name = "NetworkTablesDotNet";
+            foreach (var s in Directory.EnumerateFiles(name, "*.csproj", SearchOption.AllDirectories))
+            {
+                VersionPatcher.Patch(name, s, NTVersion + ".0");
+            }
+
+            /*
+            Process p = new Process();
+            p.StartInfo.CreateNoWindow = false;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.FileName = "nuget";
+            p.StartInfo.Arguments = $"restore {name}\\{name}.sln";
+            p.Start();
+            p.WaitForExit();
+            */
+
+            //Patch WPILib
+            if (WPILibVersion == "")
+                throw new InvalidOperationException("WPILib version not set");
+
+            name = "robotdotnet-wpilib";
+
+            foreach (var s in Directory.EnumerateFiles(name, "*.csproj", SearchOption.AllDirectories))
+            {
+                VersionPatcher.Patch(name, s, WPILibVersion + ".0");
+            }
+        }
 
         
         
-        static void Main(string[] args)
+        static void Main2(string[] args)
         {
             //First we want to download all of our packages
             #region Download NuPkg
